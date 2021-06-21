bcaecb1d90746b0b3193edd69ef75a4241d7bd8e
Scott Koranda <skoranda@gmail.com>
2021-06-21 08:20:49 -0500

    Prevent anyone but platform admin from deleting a council group
    Evolve the LigoCouncilListener to not only prevent editing a
    council group but also prevent deleting a council group.

--- Lib/LigoCouncilListener.php
+++ Lib/LigoCouncilListener.php
@@ -7,16 +7,65 @@ class LigoCouncilListener implements CakeEventListener {
 
   public function implementedEvents() {
     return array(
-      'Controller.beforeRender' => 'denyRender'
+      'Controller.beforeRender' => 'denyEdit',
+      'Controller.startup' => 'denyDelete'
     );
   }
 
-  public function denyRender(CakeEvent $event) {
+  public function denyDelete(CakeEvent $event) {
     // The subject of the event is a Controller object.
     $controller = $event->subject();
 
     // We only intend to intercept the CoGroups controller.
-    if(!$controller->name === "CoGroups") {
+    if(!($controller->name === "CoGroups")) {
+      return true;
+    }
+
+    // We only intend to intercept the delete action.
+    if(!($controller->action == "delete")) {
+      return true;
+    }
+
+    // Obtain the ID of the object being deleted.
+    $coGroupId = $controller->passedArgs[0];
+
+    // Pull the model data.
+    $args = array();
+    $args['conditions']['CoGroup.id'] = $coGroupId;
+    $args['contain'] = 'Identifier';
+
+    $group = $controller->CoGroup->find('first', $args);
+
+    // If the CoGroup has an identifier of type iscouncilgroup
+    // then set the flash and redirect to prevent rendering of the
+    // views to edit the group.
+    if(!empty($group['Identifier'])) {
+      foreach($group['Identifier'] as $identifier) {
+        if($identifier['type'] == 'iscouncilgroup') {
+
+            // Allow the platform admin to edit.
+            $roles = $controller->Role->calculateCMRoles();
+            if($roles['cmadmin']) {
+              return true;
+            }
+
+          $controller->log("Preventing delete of Council Delegate CoGroup with ID $coGroupId");
+          $controller->Flash->set(_txt('pl.ligo_council.error.noedit'), array("key" => "error"));
+          $controller->redirect($controller->referer());
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  public function denyEdit(CakeEvent $event) {
+    // The subject of the event is a Controller object.
+    $controller = $event->subject();
+
+    // We only intend to intercept the CoGroups controller.
+    if(!($controller->name === "CoGroups")) {
       return true;
     }
 
@@ -41,5 +90,7 @@ class LigoCouncilListener implements CakeEventListener {
           }
         }
     }
+
+    return true;
   }
 }
